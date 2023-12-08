913e49516dfad44d3e705a77fa01eeffe2c9fefc
Pond (satorn) <107555040+pondsan1412@users.noreply.github.com>
2023-12-09 02:00:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+// app.js
+const sleepingList = document.getElementById('sleepingList');
+let intervalId; // ใช้เก็บ ID ของ interval เพื่อให้สามารถหยุดการนับเวลาได้
 
-app = Flask(__name__)
+function startSleeping() {
+    const nameInput = document.getElementById('name');
+    const name = nameInput.value;
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    const startTime = new Date().getTime();
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    const listItem = document.createElement('li');
+    listItem.textContent = `${name} is sleeping.`;
+
+    const timerDisplay = document.createElement('div');
+    timerDisplay.className = 'timer';
+    listItem.appendChild(timerDisplay);
+
+    const stopButton = document.createElement('button');
+    stopButton.textContent = 'Stop Sleeping';
+    stopButton.onclick = function() {
+        clearInterval(intervalId); // หยุดการนับเวลาเมื่อกดปุ่มหยุด
+        const elapsedTime = (new Date().getTime() - startTime) / 1000;
+        listItem.textContent = `${name} slept for ${Math.floor(elapsedTime / 60)} minutes and ${Math.floor(elapsedTime % 60)} seconds.`;
+        stopButton.remove();
+    };
+
+    listItem.appendChild(stopButton);
+    sleepingList.appendChild(listItem);
+
+    // เริ่มต้นการนับเวลาทุก 1 วินาที
+    intervalId = setInterval(function() {
+        const elapsedTime = (new Date().getTime() - startTime) / 1000;
+        timerDisplay.textContent = `Elapsed Time: ${Math.floor(elapsedTime / 60)}m ${Math.floor(elapsedTime % 60)}s`;
+    }, 1000);
+}
